6f1eee2681d4f2de0df34b983341caebeb6c1910
hrushik98 <91076764+hrushik98@users.noreply.github.com>
2023-02-12 14:11:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import requests
 import json
 import streamlit as st
 
-response = requests.get('https://meme-api.com/gimme/sexybutnotporn')
+response = requests.get('https://meme-api.com/gimme/PublicFlashing')
 
 if response.status_code == 200:
     content = response.content.decode('utf-8')
@@ -14,6 +14,8 @@ else:
 st.set_page_config(page_title="Random Imgur Image", page_icon=":guardsman:", layout="wide")
 
 st.header("Random Imgur Image")
+if st.button("r/sexybutnotporn"):
 
-st.image(img_url, width=300)
+    st.image(img_url, width=300)
+
 
