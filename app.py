a51457e0dfdf97f4d650b8a8b4a65e845eb0a0de
hrushik98 <91076764+hrushik98@users.noreply.github.com>
2023-02-12 12:51:06 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+import requests
+import json
+import streamlit as st
+
+response = requests.get('https://meme-api.com/gimme/sexybutnotporn')
+
+if response.status_code == 200:
+    content = response.content.decode('utf-8')
+    data = json.loads(content)
+    img_url = data['url']
+else:
+    print('Failed to retrieve data')
+
+st.set_page_config(page_title="Random Imgur Image", page_icon=":guardsman:", layout="wide")
+
+st.header("Random Imgur Image")
+
+st.image(img_url, width=600)
+
