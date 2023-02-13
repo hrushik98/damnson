e536819a31ea8465b6aa5fb1d76d302ae89a2e8e
hrushik98 <91076764+hrushik98@users.noreply.github.com>
2023-02-13 13:17:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ if st.button("Go!"):
             img_url = data['url']
             st.image(img_url, width=300)
         else:
-            st.warning("Failed to retrive data. Check if the subreddit exists")
+            st.warning("Failed to retrive data. Check if the subreddit exists.")
 
 
       
