9a6bd8a2e72cbc4dbdfc6620b724d1d452bdf3b9
hrushik98 <91076764+hrushik98@users.noreply.github.com>
2023-02-13 13:04:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,30 @@
 import requests
 import json
 import streamlit as st
+st.title("Subreddit Image viewer")
+st.text("")
+st.text("Created by @Hrush1k")
+st.text("")
 
-response = requests.get('https://meme-api.com/gimme/Memes')
+sub_reddit_name = st.text_input("Enter subreddit name:")
+st.text("Example: wholesomememes for r/wholesomememes")
 
-if response.status_code == 200:
-    content = response.content.decode('utf-8')
-    data = json.loads(content)
-    img_url = data['url']
-else:
-    print('Failed to retrieve data')
+if st.button("Go!"):
+    try:
+        response = requests.get(f'https://meme-api.com/gimme/{sub_reddit_name}')
 
-st.set_page_config(page_title="Random Imgur Image", page_icon=":guardsman:", layout="wide")
+        if response.status_code == 200:
+            content = response.content.decode('utf-8')
+            data = json.loads(content)
+            img_url = data['url']
+        else:
+            print('Failed to retrieve data')
+    except:
+        st.warning("Enter a valid subreddit name!")
 
-st.header("Random meme generator")
-if st.button("generate"):
+    
 
-    st.image(img_url, width=300)
+    
 
+    st.image(img_url, width=400)
 
